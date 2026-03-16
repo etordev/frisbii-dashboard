96547977093c24314e351d643aad017bfcd01a54
etordev <enzotorella.dev@gmail.com>
2026-03-16 18:55:45 +0100

    Added navigation bar with title

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,8 @@ import { Routes } from '@angular/router';
 import { NotFoundComponent } from './core/pages/not-found/not-found.component';
 
 export const routes: Routes = [
+  // TODO: point this to CustomersListPageComponent when the page exists.
+  { path: '', component: NotFoundComponent },
   { path: 'not-found', component: NotFoundComponent },
   { path: '**', redirectTo: 'not-found' },
 ];
